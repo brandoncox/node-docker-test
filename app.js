9864e9448fa1405731f3f34620b5dcd59d7d9b20
Brandon Cox <bcox@redhat.com>
2018-07-09 10:32:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  text = "Running Node.js:" + process.versions.node
+  text = "Running Node.js app:" + process.versions.node
   text += "Mongo Servers: " + process.env.MONGODB
 
   response.end(text);
